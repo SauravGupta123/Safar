3ee4c0fe546b6bc76b350c656a24ba8e2d91bbb4
Saurav <er.sauravgpt@gmail.com>
2023-07-28 01:23:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { CssBaseline,Grid } from '@mui/material'
+import './App.css'
+import Header from './components/Header/Header'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+import List from './components/List/List'
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+      <Header/>
+      <Grid container spacing={3} style={{width:'100%'}}>
+        <Grid item xs={12} md={4}>
+            <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map/>
+        </Grid>            
+      </Grid>  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function List() {
+  return (
+    <div>
+       I am list
+    </div>
+  )
+}
+
+export default List
