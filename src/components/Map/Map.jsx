9842578e039971a74682445973c77cdd4f47f010
Saurav <er.sauravgpt@gmail.com>
2023-08-04 13:32:52 +0530

    google map inserted along with the coordinates given by app.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@mui/material';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Rating from '@mui/lab/Rating';
 
-function Map() {
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
+
+const Map = ({setCoordinates,setBounds,coordinates}) => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  
+
+
   return (
-    <div>
-       I am map
-    </div>
-  )
-}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAsJufb2ilCHs7WktSoPfd95oIvY81TU4M" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={ '' }
+        onChange={(e)=>{
+          setCoordinates({lat:e.center.lat,lng:e.center.lng})
+          setBounds({ne:e.marginBounds.ne, sw:e.marginBounds.sw})
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
 
-export default Map
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
