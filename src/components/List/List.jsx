41b63e8bd8b4a57410b13a6b580db3dc555165f8
Saurav <er.sauravgpt@gmail.com>
2023-08-04 13:35:28 +0530

    ADDED:  search filter and rating filter, name of the resturants which are fetched by the api

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@mui/material';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
-function List() {
+const List = ({places}) => {
+  // const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
+  const [type, setType] = useState('resturants');
+  const [rating, setRating] = useState('');
+
   return (
-    <div>
-       I am list
-    </div>
-  )
-}
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <MenuItem value='resturants'>Resturants</MenuItem>
+          <MenuItem value='hotels'>Hotels</MenuItem>
+          <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        >
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place,i)=>(
+          <Grid item key={i} xs={12}>
+             <PlaceDetails place={place}/> 
+          </Grid> 
+        ))}
+      </Grid>
 
-export default List
+
+
+
+    </div>
+  );
+};
+
+export default List;
