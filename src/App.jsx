f1cb6db2f3c9c18a8248950ab81e5914490cdff4
Saurav <er.sauravgpt@gmail.com>
2023-08-04 13:37:21 +0530

    INSERTED: state variables like places,cordinates. fetching realTime users location,and manage re-rendering of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import { CssBaseline,Grid } from '@mui/material'
 import './App.css'
 import Header from './components/Header/Header'
 import Map from './components/Map/Map'
 import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 import List from './components/List/List'
+import { getPlaceData } from './api'
 
 function App() {
+const [places, setPlaces]= useState([])
+const [coordinates,setCoordinates]=useState({});
+const [bounds,setBounds]=useState({sw:"", ne: ""});
+useEffect(()=>{
+  navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
+    setCoordinates({lat:latitude,lng:longitude})
+  })
+},[]);
+
+  useEffect(()=>{
+    getPlaceData(bounds.sw, bounds.ne).then((data)=>{
+      console.log(data);
+        setPlaces(data);
+    })
+  },[coordinates,bounds])
 
   return (
     <>
@@ -14,10 +30,14 @@ function App() {
       <Header/>
       <Grid container spacing={3} style={{width:'100%'}}>
         <Grid item xs={12} md={4}>
-            <List/>
+            <List places={places}/>
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map/>
+          <Map 
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>            
       </Grid>  
     </>
