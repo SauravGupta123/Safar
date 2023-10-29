fbf146c0f0f9141d971293ea354b8de92e8f75ec
Saurav <er.sauravgpt@gmail.com>
2023-10-29 15:36:53 +0530

    resturant pic added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@mui/material';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import Rating from '@mui/lab/Rating';
-
+import resturant from '../../assets/resturant.jpeg'
 import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
 
@@ -27,38 +27,40 @@ const Map = ({setCoordinates,setBounds,coordinates,places}) => {
           setCoordinates({lat:e.center.lat,lng:e.center.lng})
           setBounds({ne:e.marginBounds.ne, sw:e.marginBounds.sw})
         }}
-          onChildClick={(child) => setChildClicked(child)}
+          onChildClick={(child) => 
+            setChildClicked(child)
+          }
       >
 
-        {console.log(places)}
+      
         {
-        // places?.map((place,i)=>{
-        //   <div 
-        //     className={classes.markerContainer}
-        //     lat={Number(place.latitude)}
-        //     lng={Number(place.longitude)}
-        //     key={i}
-        //   >
-        //     {/* {console.log(isDesktop)} */}
-        //     {!isDesktop?
-        //       (
-        //       <LocationOnOutlinedIcon color='primary' fontSize='large' />
-        //       )
-        //     :(
-        //       <Paper elevation={3} className={classes.paper}>
-        //       <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
-        //       <img
-        //         className={classes.pointer}
-        //         src={place.photo ? place.photo.images.large.url :{}}
-        //       />
-        //       <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
-        //     </Paper>
-        //     )}
+  places?.map((place, i) => (
+    <div
+      className={classes.markerContainer}
+      lat={Number(place.latitude)}
+      lng={Number(place.longitude)}
+      key={i}
+    >
+      {/* {console.log(place.latitude)} */}
+      {!isDesktop ? (
+        <LocationOnOutlinedIcon color="primary" fontSize="large" />
+      ) : (
+        <Paper elevation={3} className={classes.paper}>
+          <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+            {place.name}
+          </Typography>
+          <img
+            className={classes.pointer}
+            src={place.photo ? place.photo.images.large.url : resturant}
+            alt={place.name}
+          />
+          <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+        </Paper>
+      )}
+    </div>
+  ))
+}
 
-        //   </div>
-          
-        // })
-        }
 
       </GoogleMapReact>
     </div>
